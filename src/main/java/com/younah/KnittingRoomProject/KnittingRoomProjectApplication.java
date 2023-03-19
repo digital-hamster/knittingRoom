5da253fbdfc4928f30e0214f1d9bf9c1b67644b6
digital-hamster <waterlove1439@naver.com>
2023-03-19 19:32:01 +0900

    start

---
+++ src/main/java/com/younah/KnittingRoomProject/KnittingRoomProjectApplication.java
@@ -0,0 +1,13 @@
+package com.younah.KnittingRoomProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnittingRoomProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnittingRoomProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/younah/KnittingRoomProject/KnittingRoomProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.younah.KnittingRoomProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnittingRoomProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
